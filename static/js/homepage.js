e0f63019ef3a31934663c1c37a597a9d48075d35
Laurel Mossor <laurel.mossor@gmail.com>
2022-04-29 16:56:14 -0700

    on click, country displays name at top

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,6 +1,14 @@
 "use strict";
 
-////// Key document elements //////
+/** This visualization was made possible by utilizing and altering code from the following examples:
+    * Choropleth map with hover effect in d3.js by Yan Holtz from: 
+        * https://d3-graph-gallery.com/graph/choropleth_hover_effect.html
+    * World Map Centered v7 by d3noob, released under the MIT license from:
+        *  https://bl.ocks.org/d3noob/82f4db23d47971c74699abb5f4bf8204
+ */
+
+
+////////////////// Key document elements /////////////
 const dropDown = document.querySelector('select[name="country-dropdown"]');
 const countryCodeSelectButton = document.getElementById("country-code-selector");
 const newsDiv = document.querySelector("#news-dashboard");
@@ -8,7 +16,7 @@ const dashboard = document.querySelector("#dashboard-parent");
 const countryInfoDiv = document.querySelector("#country-info");
 
 
-/////// HELPER Functions for the Button Event /////////
+//////////// HELPER Functions for the Button Event ///////////////
 
 /** Create and call the REST Countries API */
 function restCountriesCall(twoDigCountryCode) {
@@ -42,7 +50,6 @@ function countryNameNewsAPIcall(twoDigCountryCode) {
         });        
 }
 
-
 /** Refresh the page so that the blocks are cleared */
 function refreshPage() {
     // Clear previous country
@@ -66,28 +73,55 @@ countryCodeSelectButton.addEventListener("click", (evt) => {
 });
 
 
+//////////////////////////// Map specs /////////////////////////
+let width = 850;
+let height = 520;
 
+const projection = d3.geoMercator()
+    .center([0, 20])
+    .scale(120)
+    .rotate([-10,0]);
 
+const svg = d3.select("#clickable-map");
+    svg.attr("width", width);
+    svg.attr("height", height);
+    svg.attr("viewBox", [0,0,width,height]);
 
-// function RenderNews() {
-//     // Country code choice as state?
-//     const [news, showNews] = React.useState([]);
+const path = d3.geoPath()
+    .projection(projection);
 
-//     React.useEffect(() => {
-//         fetch("/news-country-test")
-//             .then((response) => response.json())
-//             .then((result) => {
-//                 showNews(result);
-//             });
-//     }, []);
+const g = svg.append("g");
 
-//     const newsList = [news];
+/////// Zoom function ////////
+svg.call(d3.zoom()
+    .extent([[0,0], [width, height]])
+    .scaleExtent([1,8])
+    .on("zoom", (event) => {
+        g.attr("transform", event.transform);
+}
+));
 
-//     return <p>{newsList}</p>
-// }
+//////////////////////////// Load and display the World /////////////////////
+d3.json("static/map-files/world-map-TOPO.json")
+    .then(function(data) {
+        const sovreignCountries = topojson.feature(data, data.objects.ne_10m_admin_0_sovereignty);
+        g.selectAll("path")
+            .data(sovreignCountries.features)
+            .enter().append("path")
+            .attr("d", path)
+            .attr("class", "Country")
+            .attr("id", function(d) {
+            return `${d.properties.NAME}`
+            })
+            .on("click", function(evt) {
+                // evt.target.id is the country name, and that's how we'll call the APIs
+                // I want a box to show up with a tooltip:
+                    // Displays country name, and a button that says "Activate Dashboard"
+                const countryName = evt.target.id;
+                d3.select("#country-name-box").text(`${countryName}`);
 
+            })
+            .append("title").text(function(d) {return `${d.properties.NAME}`;})
+            ;
+        });
 
-// ReactDOM.render(<RenderNews />, 
-// document.querySelector('#news-dashboard'));
-
-// Practice just by rendering the country code at the bottom
