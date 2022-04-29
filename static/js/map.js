e203bb284bfe24f3d3734f8b23bd7370c47d2ee0
Laurel Mossor <laurel.mossor@gmail.com>
2022-04-29 15:04:59 -0700

    got a clickable map! shows title of country on hover, and can zoom

--- static/js/map.js
+++ static/js/map.js
@@ -1,103 +1,59 @@
 "use strict";
 
+/** This visualization was made possible by utilizing and altering code from the following examples:
+    * Choropleth map with hover effect in d3.js by Yan Holtz from: 
+        * https://d3-graph-gallery.com/graph/choropleth_hover_effect.html
+    * World Map Centered v7 by d3noob, released under the MIT license from:
+        *  https://bl.ocks.org/d3noob/82f4db23d47971c74699abb5f4bf8204
+ */
+
 
 // "/static/map-files/world-map-Geo.geojson"
 // "/static/map-files/map-topojson-LITE.json"
 // static/map-files/world-map-TOPO.json
 
 
-// let width = 850;
-// let height = 520;
-
-// const projection = d3.geoMercator()
-//     .center([0, 20])
-//     .scale(120)
-//     .rotate([-10,0]);
-
-// const svg = d3.select("#clickable-map");
-//     svg.attr("width", width);
-//     svg.attr("height", height);
-
-// const path = d3.geoPath()
-//     .projection(projection);
-
-// const g = svg.append("g");
-
-// // load and display the World
-// d3.json("static/map-files/world-map-TOPO.json")
-//     .then(function(topology) {
-//         g.selectAll("path")
-//             .data(topojson.feature(topology, topology.objects.ne_10m_admin_0_sovereignty).features)
-//             .enter().append("path")
-//             .attr("d", path)
-//             .attr("fill", "grey")
-//             .attr("stroke", "white")
-//             .attr("stroke-width", "0.5px");
-            
-//         }
-
-// )
-// // event listeners here?
-// ;
-
-// PLAYING AROUND!!!
-
-let width = 600;
-let height = 400;
-// let myMap = new Map()
+let width = 850;
+let height = 520;
 
 const projection = d3.geoMercator()
     .center([0, 20])
-    .scale(100)
-    // .rotate([-10,0])
-    .translate([width / 2, height / 2]);
-    
-const path = d3.geoPath()
-.projection(projection);
-    
+    .scale(120)
+    .rotate([-10,0]);
+
 const svg = d3.select("#clickable-map");
     svg.attr("width", width);
     svg.attr("height", height);
+    svg.attr("viewBox", [0,0,width,height]);
 
+svg.call(d3.zoom()
+    .extent([[0,0], [width, height]])
+    .scaleExtent([1,8])
+    .on("zoom", (event) => {
+        g.attr("transform", event.transform);
+}
+));
+
+const path = d3.geoPath()
+    .projection(projection);
 
 const g = svg.append("g");
 
 // load and display the World
-d3.json("/static/map-files/world-map-Geo.geojson")
-    .then(function(geoJSONdata) {
+d3.json("static/map-files/world-map-TOPO.json")
+    .then(function(data) {
+        const countries = topojson.feature(data, data.objects.ne_10m_admin_0_sovereignty);
         g.selectAll("path")
-            .data(geoJSONdata.features)
-            .enter()
-            .append("path")
-            // Draw each country
+            .data(countries.features)
+            .enter().append("path")
             .attr("d", path)
-            .attr("fill", "transparent")
-            .attr("stroke", "black")
-            .attr("stroke-width", "0.25px")
-            .attr("name", (d) => {d.properties.NAME})
             .attr("class", "Country")
-            .attr("name", (d, i) => {i});
-            // .on("click", (evt) => {
-            //     let x = d3.selectAll(".Country")
-            //     console.log(x._groups[0]);
-            // });
-            
-        
-            
-        }
+            .append("title").text(function(d) {return `${d.properties.NAME}`;})
+        });
 
-)
-;
-// const allCountriesClass = d3.selectAll(".Country")
 
-// for (const country of allCountriesClass){
+let allCountries = d3.selectAll(".Country")
 
-//     country.addEventListener("click" , (evt) =>{
-//         evt.preventDefault();
 
-//         console.log(evt.target);
-//     })
-// };
-// console.log(d3.map["US"])
 
 
