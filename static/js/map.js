fb6364880b2aadbcd9fb6b1a641ad28bc512ac0b
Laurel Mossor <laurel.mossor@gmail.com>
2022-04-28 18:21:52 -0700

    getting map worked out

--- static/js/map.js
+++ static/js/map.js
@@ -1,43 +1,103 @@
 "use strict";
 
 
-
-// "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
-// "/static/map-files/world-map-Geo.geojsonj"
-
-
+// "/static/map-files/world-map-Geo.geojson"
+// "/static/map-files/map-topojson-LITE.json"
+// static/map-files/world-map-TOPO.json
 
 
+// let width = 850;
+// let height = 520;
 
-        let width = 960;
-        let height = 500;
+// const projection = d3.geoMercator()
+//     .center([0, 20])
+//     .scale(120)
+//     .rotate([-10,0]);
 
-        const projection = d3.geoMercator()
-        .center([0, 5 ])
-        .scale(150)
-        .rotate([-180,0]);
+// const svg = d3.select("#clickable-map");
+//     svg.attr("width", width);
+//     svg.attr("height", height);
 
-        const svg = d3.select("#clickable-map");
-            svg.attr("width", width);
-            svg.attr("height", height);
+// const path = d3.geoPath()
+//     .projection(projection);
 
-        const path = d3.geoPath()
-            .projection(projection);
+// const g = svg.append("g");
 
-        const g = svg.append("g");
+// // load and display the World
+// d3.json("static/map-files/world-map-TOPO.json")
+//     .then(function(topology) {
+//         g.selectAll("path")
+//             .data(topojson.feature(topology, topology.objects.ne_10m_admin_0_sovereignty).features)
+//             .enter().append("path")
+//             .attr("d", path)
+//             .attr("fill", "grey")
+//             .attr("stroke", "white")
+//             .attr("stroke-width", "0.5px");
+            
+//         }
+
+// )
+// // event listeners here?
+// ;
+
+// PLAYING AROUND!!!
+
+let width = 600;
+let height = 400;
+// let myMap = new Map()
+
+const projection = d3.geoMercator()
+    .center([0, 20])
+    .scale(100)
+    // .rotate([-10,0])
+    .translate([width / 2, height / 2]);
+    
+const path = d3.geoPath()
+.projection(projection);
+    
+const svg = d3.select("#clickable-map");
+    svg.attr("width", width);
+    svg.attr("height", height);
+
+
+const g = svg.append("g");
 
 // load and display the World
-        d3.json("/static/map-files/map-topojson-LITE.json").then(function(topology) {
+d3.json("/static/map-files/world-map-Geo.geojson")
+    .then(function(geoJSONdata) {
+        g.selectAll("path")
+            .data(geoJSONdata.features)
+            .enter()
+            .append("path")
+            // Draw each country
+            .attr("d", path)
+            .attr("fill", "transparent")
+            .attr("stroke", "black")
+            .attr("stroke-width", "0.25px")
+            .attr("name", (d) => {d.properties.NAME})
+            .attr("class", "Country")
+            .attr("name", (d, i) => {i});
+            // .on("click", (evt) => {
+            //     let x = d3.selectAll(".Country")
+            //     console.log(x._groups[0]);
+            // });
+            
+        
+            
+        }
 
-            g.selectAll("path")
-            .data(topojson.feature(topology, topology.objects.ne_10m_admin_0_countries).features)
-            .enter().append("path")
-            .attr("d", path);
+)
+;
+// const allCountriesClass = d3.selectAll(".Country")
 
-        })
-        // event listeners here?
-        ;
+// for (const country of allCountriesClass){
 
+//     country.addEventListener("click" , (evt) =>{
+//         evt.preventDefault();
 
+//         console.log(evt.target);
+//     })
+// };
+// console.log(d3.map["US"])
 
 
